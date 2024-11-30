564ea07c9b90f32aa3e3052ec42f9d968fcbd6d8
ndoherty <ndoherty.xyz@gmail.com>
2024-11-30 00:20:36 -0700

    add api url to frontend

--- apps/web/src/constants/server.ts
+++ apps/web/src/constants/server.ts
@@ -1,4 +1,6 @@
 export const API_URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+  process.env.NODE_ENV === "production"
+    ? "https://higherchatapi-production.up.railway.app"
+    : "http://localhost:8080";
 
 export const GRAPHQL_URL = `${API_URL}/api/graphql`;
